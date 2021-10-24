16c99a9c43972494376b3a33f1abccf91b2a956f
jinieeee <lje911@gmail.com>
2021-10-25 00:13:45 +0900

    dataStructure commit and push

---
+++ src/hackerRank/prepare/java/DataStructures/OneDArray.java
@@ -0,0 +1,25 @@
+package hackerRank.prepare.java.DataStructures;
+
+import java.util.Scanner;
+
+public class OneDArray {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        
+        for(int i = 0; i < a.length; i++) {
+        	a[i] = scan.nextInt();
+        }
+        
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+	}
+
+}
---
+++ src/hackerRank/prepare/java/DataStructures/OneDArray_2.java
@@ -0,0 +1,49 @@
+package hackerRank.prepare.java.DataStructures;
+
+import java.util.Scanner;
+
+public class OneDArray_2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+	}
+	
+	public static boolean canWin(int leap, int[] game) {
+		// test case 1 ~ 오류 발생
+		boolean result = false;
+        for(int i = 0; i < game.length - 1; i++) {
+            if(i + leap > game.length) {
+                result = true;
+                break;
+            } else if(i + 1 == game.length || i + leap == game.length){
+                if(game[i + 1] == 0 || game[i + leap] == 0) {
+                    result = true;
+                    break;
+                }
+            } else {
+                if(game[i + 1] != 0) {
+                    if(game[i + leap] != 0) {
+                        result = false;
+                        break;
+                    } else {
+                        i += leap - 1;
+                    }
+                }                
+            }
+        }
+        return result;
+    }
+}
