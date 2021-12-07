3a148a330fc2475fbac54dd5a5f8a0a6e7bc8488
jinieeee <lje911@gmail.com>
2021-12-07 23:07:23 +0900

    SuperReducedString stack() 활용

--- src/hackerRank/Algorithms/Strings/SuperReducedString.java
+++ src/hackerRank/Algorithms/Strings/SuperReducedString.java
@@ -1,5 +1,8 @@
 package hackerRank.Algorithms.Strings;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class SuperReducedString {
 
 	public static void main(String[] args) {
@@ -8,28 +11,51 @@ public class SuperReducedString {
 	}
 	
 	public static String superReducedString(String s) {
+		/* 예외가 너무 많이 발생
+		 * char[] ch = s.toCharArray(); String result = ""; int count = 0;
+		 * 
+		 * for(int i = 0; i < ch.length; i++) { if(i == 0) { count += 1; } else {
+		 * if(ch[i - 1] == ch[i]) { count += 1; } else { result += count % 2 == 0? "" :
+		 * ch[i - 1]; count = 1; } } if(i == ch.length - 1) { result += count % 2 == 0?
+		 * "" : ch[i - 1]; } System.out.println(result); }
+		 * 
+		 * return result.equals("")? "Empty String" : result;
+		 */
+		
+		/* 다른 방법 */
+		Stack<Character> stack = new Stack<>();
 		
 		char[] ch = s.toCharArray();
-		String result = "";
-		int count = 0;
 		
 		for(int i = 0; i < ch.length; i++) {
 			if(i == 0) {
-				count += 1;
+				stack.push(ch[i]);
 			} else {
-				if(ch[i - 1] == ch[i]) {
-					count += 1;
+				if(!stack.empty() && stack.peek() == ch[i]) {
+					stack.pop();
 				} else {
-					result += count % 2 == 0? "" : ch[i - 1];
-					count = 1;
+					stack.push(ch[i]);
 				}
 			}
-			if(i == ch.length - 1) {
-				result += count % 2 == 0? "" : ch[i - 1];
-			}
-			System.out.println(result);
 		}
-		
-		return result.equals("")? "Empty String" : result;
+		String result = "";
+		for(Character c : stack) {
+			result += c;
+		}
+		return stack.empty()? "Empty String" : result;
 	}
 }
+
+
+/* Discussion 참고
+ * for (int i=1; i<s.length(); i++){
+        if(s.charAt(i) == s.charAt(i-1)){
+            s= s.substring(0 , i-1) + s.substring(i+1);
+            i=0;
+        }
+    } 
+    return s.length()==0? "Empty String" : s ;
+    
+    i번째 문자와 i-1번째 문자가 같으면 잘라내고 (i+1)번째 문자부터 끝까지를 더해 새로운 문자열 생성(같은 문자 2개를 지우기)
+    & i값 초기화
+ */
