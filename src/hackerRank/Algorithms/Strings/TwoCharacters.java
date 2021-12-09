90903d016e15bb1ae1cedca7b7df99062cee558a
jinieeee <lje911@gmail.com>
2021-12-09 23:34:31 +0900

    unsuccess

---
+++ src/hackerRank/Algorithms/Strings/TwoCharacters.java
@@ -0,0 +1,34 @@
+package hackerRank.Algorithms.Strings;
+
+import java.util.Arrays;
+
+public class TwoCharacters {
+
+	public static void main(String[] args) {
+		System.out.println(alternate("beabeefeab"));
+	}
+	
+	public static int alternate(String s) {
+//		int[] arr = new int[26];
+//		int result = 0;
+//	    for(int i = 0; i < s.length(); i++) {
+//	    	int index = (int)s.charAt(i) - 97;
+//	    	arr[index] += 1;
+//	    }
+//	    
+//	    Arrays.sort(arr);
+//	    int count = 0;
+//	    for(int i = 0; i < arr.length; i++) {
+//	    	if(count >= 2) {
+//	    		result += arr[i];
+//	    	}
+//	    	if(arr[i] != 0) {
+//	    		count += 1;
+//	    		arr[i] = 0;
+//	    	}
+//	    }
+//	    return result;
+		
+		return 0;
+	}
+}
