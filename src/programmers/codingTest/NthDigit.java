cdf40125d3b42c2c489b4993de88c44e5b0c964a
jinieeee <lje911@gmail.com>
2021-12-11 19:43:56 +0900

    주석 추가

--- src/programmers/codingTest/NthDigit.java
+++ src/programmers/codingTest/NthDigit.java
@@ -26,7 +26,8 @@ public class NthDigit {
         // 115 번째 구하는 경우 115 - 10 = 105
         n -= sum + 1;
         // 자리수의 총 숫자 갯수  + 몇 번째 숫자인지
-        String str = String.valueOf((int)Math.pow(10, num2) + n / (num2 + 1));	// str : 62
+        String str = Integer.toString((int)Math.pow(10, num2) + n / (num2 + 1)); 
+        // String str = String.valueOf((int)Math.pow(10, num2) + n / (num2 + 1));	// str : 62
         // 0 ~ 9 : 아스키코드 48 ~ 57
         // 그러므로 62의 1번째 2인 문자를 int로 바꾸면 50이므로 48('0')을 빼서 정확한 숫자를 알아낸다
         return str.charAt(n % ((int)num2 + 1)) - '0';
