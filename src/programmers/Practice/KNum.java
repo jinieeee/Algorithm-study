2d52e147877d8b4c7d7811f366052b8266208785
jinieeee <lje911@gmail.com>
2021-12-17 15:37:56 +0900

    commit

---
+++ src/programmers/Practice/KNum.java
@@ -0,0 +1,20 @@
+package programmers.Practice;
+
+public class KNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int num = 0; num < commands.length; num++) {
+        	int i = commands[num][0];
+        	int[] newArr = new int[commands[num][1]];
+        	
+        }
+        return answer;
+    }
+}
