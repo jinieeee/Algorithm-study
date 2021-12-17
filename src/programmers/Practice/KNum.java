581df6c31b7e4265f9b46a7246bfd2c10602885b
jinieeee <lje911@gmail.com>
2021-12-17 16:45:26 +0900

    commit

--- src/programmers/Practice/KNum.java
+++ src/programmers/Practice/KNum.java
@@ -1,19 +1,36 @@
 package programmers.Practice;
 
+import java.util.Arrays;
+
 public class KNum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		System.out.println(solution(array, commands));
 	}
 
-	public int[] solution(int[] array, int[][] commands) {
+	public static int[] solution(int[] array, int[][] commands) {
+		int count = 0;
         int[] answer = new int[commands.length];
-        
-        for(int num = 0; num < commands.length; num++) {
-        	int i = commands[num][0];
-        	int[] newArr = new int[commands[num][1]];
+        while(count != commands.length) {
+        	int i = commands[count][0] - 1;	// index : 1
+        	int j = commands[count][1];	// index : 4
+        	int k = commands[count][2] - 1;	// index : 2
+        	int size = j - i;
+        	int[] newArr = new int[size];
+        	for(int num = 0; num < size; num++) {
+        		// System.out.println(array[i]);
+        		newArr[num] = array[i];
+        		++i;
+        	}
+        	Arrays.sort(newArr);
+        	// System.out.println(Arrays.toString(newArr));
+        	// System.out.println(k);
         	
+        	answer[count] = newArr[k];
+        	count++;
         }
         return answer;
     }
