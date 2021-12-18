6e06bf015995a5fb662e66150985958e4ab51ab5
jinieeee <lje911@gmail.com>
2021-12-18 23:15:49 +0900

    commit

--- src/programmers/Practice/FunctionDev.java
+++ src/programmers/Practice/FunctionDev.java
@@ -1,5 +1,9 @@
 package programmers.Practice;
 
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
+
 public class FunctionDev {
 
 	public static void main(String[] args) {
@@ -7,10 +11,25 @@ public class FunctionDev {
 
 	}
 	
-	// 작업의 순서가 적힌 progresses 배열과 각 작업별 진행률이 적힌 speeds 배열
+	// 작업의 순서와 진행률이 적힌 progresses 배열과 각 작업별 개발속도가 적힌 speeds 배열
 	// 각 배포마다 몇 개의 기능이 배포되는지 return
 	public static int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+		int size = progresses.length;
+        int[] answer = new int[size];
+        boolean end = true;
+        
+        Queue<Integer> q = null;
+        for(int i = 0; i < size; i++) {
+        	q.add(progresses[i]);
+        }
+        
+        while(end) {
+        	for(int i = 0; i < size; i++) {
+        		int a = q.peek();
+        		q.add(a + speeds[i]);
+        	}
+        }
+        
         return answer;
     }
 }
