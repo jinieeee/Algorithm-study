31213d3c6a5752944683e89f47c390d2adbb2b47
jinieeee <lje911@gmail.com>
2021-12-18 23:05:35 +0900

    문제 commit

---
+++ src/programmers/Practice/FunctionDev.java
@@ -0,0 +1,16 @@
+package programmers.Practice;
+
+public class FunctionDev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// 작업의 순서가 적힌 progresses 배열과 각 작업별 진행률이 적힌 speeds 배열
+	// 각 배포마다 몇 개의 기능이 배포되는지 return
+	public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
