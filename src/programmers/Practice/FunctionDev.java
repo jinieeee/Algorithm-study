561be4e9fe12d59fb34e9864fdedc5b37cb994e1
jinieeee <lje911@gmail.com>
2021-12-18 23:20:11 +0900

    commit

--- src/programmers/Practice/FunctionDev.java
+++ src/programmers/Practice/FunctionDev.java
@@ -13,6 +13,8 @@ public class FunctionDev {
 	
 	// 작업의 순서와 진행률이 적힌 progresses 배열과 각 작업별 개발속도가 적힌 speeds 배열
 	// 각 배포마다 몇 개의 기능이 배포되는지 return
+	// ex. 진행률 = {93, 30, 55}, 개발속도 = {1, 30, 5}
+	// 7일뒤 2개(1,2번째), 9일뒤 2개(3번째)
 	public static int[] solution(int[] progresses, int[] speeds) {
 		int size = progresses.length;
         int[] answer = new int[size];
@@ -26,7 +28,8 @@ public class FunctionDev {
         while(end) {
         	for(int i = 0; i < size; i++) {
         		int a = q.peek();
-        		q.add(a + speeds[i]);
+        		int b = a + speeds[i];
+        		q.add(b);
         	}
         }
         
