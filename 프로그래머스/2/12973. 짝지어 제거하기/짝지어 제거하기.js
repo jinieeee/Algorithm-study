62d8ec7da34ed2b152263cc4ad632cff964b6bd6
jinieeee <89566406+jinieeee@users.noreply.github.com>
2025-06-17 23:00:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.84 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,13 +1,15 @@
 function solution(s) {
     let stack = [];
     
-    stack.push(s[0]);
-    for(let i = 1; i < s.length; i++) {
-        const pre = stack.length > 0 ? stack.pop() : null;
-        if(!pre) {
+    for(let i = 0; i < s.length; i++) {
+        if(stack.length === 0) {
+            // stack pop 불가하면 무조건 push
             stack.push(s[i]);
-        } else if(pre !== s[i]) {
-            stack.push(pre);
+        } else if (stack[stack.length - 1] === s[i]) {
+            // stack 마지막 값과 s[i] 값이 일치하면 제거 가능
+            stack.pop();
+        } else {
+            // 제거 불가하면 stack push
             stack.push(s[i]);
         }
     }
